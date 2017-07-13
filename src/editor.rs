c48ebb4c51332dbdbb85ceb2451ab22d531542d0
dobkeratops <dobkeratopsgit@gmail.com>
2017-07-13 05:18:01 +0100

    ok

--- src/editor.rs
+++ src/editor.rs
@@ -1,22 +1,46 @@
 use super::*;
 use ::bsp::bspdraw as draw;
+use window as w;
 type V3=(f32,f32,f32);
 
+struct Cam{
+    pos:V3,zoom:f32,
+}
+
+type VertexIndex=i32;
+///
 pub struct Editor {
-    lines:Vec<(V3,V3)>,
+    vertices:Vec<(V3)>,
+    edges:Vec<[VertexIndex;2]>,
+    cam:Cam,
     tool:Tool,
-    drag:Drag
+    state:ES
 }
 
-pub fn new() -> sto<window::State> {
+///todo: grow a MeshEditor interface.
+impl Editor{
+    fn add_vertex(&mut self,p:V3)->VertexIndex{
+        let vi=self.vertices.len();
+        self.vertices.push(p);
+        vi as VertexIndex
+    }
+    fn add_edge(&mut self,a:VertexIndex,b:VertexIndex){
+        self.edges.push([a,b]);
+    }
+}
+
+pub fn new() -> sto<w::State> {
     new!(Editor{
-        lines: vec![],
+        vertices: vec![(-0.5f32,0.0f32,0.5f32),(0.5f32,-0.5f32,0.5f32),
+                    (-1.0,-1.0,0.0),(1.0,1.0,0.0)],
+        edges:vec![[0,1],[2,3]],
+        cam:Cam{pos:(0.0,0.0,0.0),zoom:1.0},
         tool:Tool::DrawLine,
-        drag:Drag::None,
-    }=>window::State)
+        state:ES::None,
+    }=>w::State)
 }
 
-type DragStart=window::KeyAt;
+type DragStart=w::KeyAt;
 type AfterDrag=Tool;
 
 #[derive(Clone,Debug,Copy)]
@@ -30,35 +54,62 @@ enum Tool {
 impl Default for Tool{fn default()->Self{Tool::DrawLine}}
 
 #[derive(Clone,Debug,Copy)]
-enum Drag{
+enum ES{
     None,
+    LastPoint(i32),
     Move(DragStart),
     Line(DragStart),
     Rect(DragStart),
 }
-impl Default for Drag{fn default()->Self{Drag::None}}
+impl Default for ES{fn default()->Self{ES::None}}
 
-impl window::State for editor::Editor{
-    fn key_mappings(&mut self,f:&mut window::KeyMappings){
+impl w::State for Editor{
+    fn key_mappings(&mut self,f:&mut w::KeyMappings){
         f('\x1b',"back",  &mut ||Flow::Pop());
         f('1',"add obj",  &mut ||Flow::Pop());
         f('2',"add trigger", &mut ||Flow::Pop());
     }
 
-    fn on_key_down(&mut self,p:window::KeyAt)->window::Flow{
-        match p.0 {
+    fn on_key(&mut self,p:w::KeyAt)->w::Flow{
+        match (p.0,p.1) {
             //s
-            '\x1b'=>Flow::Pop(),
-            _=>Flow::Continue()
+            ('\x1b',true)=>w::Pop(),
+            _=>w::Continue()
         }
     }
     fn render(&self,_:f32){
-        for l in self.lines.iter(){
-            draw::line(&l.0, &l.1, 0xff00ff00)
+        for l in self.edges.iter(){
+            draw::line(&self.vertices[l[0] as usize], &self.vertices[l[1] as usize], 0xff00ff00)
         }
-        ::bsp::bspdraw::main_mode_text("editor");
+        draw::main_mode_text("editor");
+    }
+    fn event(&mut self, e:w::Event)->w::Flow{
+        println!("event:");dump!(e);
+        match e{
+            w::MouseButton(w::LeftButton,true,(x,y))=>{
+                let ve=self.add_vertex((x,y,0.0f32));
+                match self.state{
+                    ES::LastPoint(vs)=> self.add_edge(ve,vs),
+                    _=>{}
+                }
+                self.state=ES::LastPoint(ve);
+            },
+            w::MouseButton(w::RightButton,true,(x,y))=> {
+                self.state=ES::None;
+            },
+            _=>{}
+        }
+
+        w::Continue()
     }
 }
+
+/*
+trait Pane {
+    fn size(&self)->w::PixelSize;
+
+}
+*/
 
 // map view may rend
 
--- src/window.rs
+++ src/window.rs
@@ -1,118 +1,25 @@
 use super::*;
 pub use Flow::*;
 use std::path;
+use ::bsp::bspdraw;
 
-
-//type sto<T>=Rc<RefCell<T>>;
+//type sto<T>=Rc<RefCell<T>>;g_mo
 pub type sto<T>=Box<T>; //shared trait object
 
-//type sp<T> = Rc<RefCell<T>>;
+//type sp<T> = Rc<RekeyfCell<T>>;
 //fn new_sp<T>()
 
 // glut/whatever interaction, and handles states.
 pub type KeyCode=char;
 
 #[derive(Debug,Clone,Copy)]
-pub struct KeyAt(pub KeyCode,pub ScreenPos);
-
-pub type ScreenPos=[i32;2];
-static mut g_key:[bool;256]=[false;256];
-static mut g_mouse_button:i32=0;
-static mut g_mouse_pos:ScreenPos=[0,0];
-static mut g_joystick:((f32,f32),u32)=((0.0f32,0.0f32),0);
-static mut g_keypress_pos:[i32;2]=[0,0];
-static mut g_keypress:Option<u8>=None;
-static mut g_screensize:ScreenPos=[1024,1024];
-const  MaxEvent:usize=256;
-static mut g_ui_event:[Event;MaxEvent]=[Event::None;MaxEvent];
-static mut g_head:i32=0;
-static mut g_tail:i32=0;
-
-pub fn keyboard_func(key:u8, x:i32,y:i32){
-    unsafe{g_keypress=Some(key); g_key[key as usize]=true;g_keypress_pos=[x,y];}
-}
-pub fn special_func_sub(key:GLuint, s:bool,x:i32,y:i32){
-    assert!((key as u32&0xff) == key as u32);
-    unsafe{
-        if s{ g_keypress=Some(key as u8); }
-        g_key[key as usize]=s;
-        g_keypress_pos=[x,y];
-    }
-}
-pub fn reshape_func(x:i32,y:i32){
-    println!("resizing..");
-    unsafe{g_screensize=[x,y];}
-}
-pub fn screen_size()->ScreenPos{ unsafe{g_screensize} }
-
-pub fn special_func(key:GLuint, x:i32,y:i32){
-    special_func_sub(key,true,x,y);
-}
-pub fn special_up_func(key:GLuint, x:i32,y:i32){
-    special_func_sub(key,false,x,y);
-}
-
-//pub fn modifiers()->GLuint{glutGetModifiers()}
-
-pub fn keyboard_up_func(key:u8, x:i32,y:i32){
-    unsafe{g_key[key as usize]=false;}
-}
-pub fn set_mouse_pos(x:i32,y:i32){unsafe{g_mouse_pos=[x,y]};}
-pub fn push_event(e:Event){
-    unsafe {
-        let next = (g_head + 1) & ((MaxEvent - 1) as i32);
-        if next != g_tail {
-            g_ui_event[g_head as usize] = e;
-            g_head=next;
-        } else {
-            println!("buffer full,lost event");
-        }
-    }
-}
-pub fn pop_event()->Option<Event>{
-    unsafe {
-        let next = (g_tail + 1) & ((MaxEvent - 1) as i32);
-        if g_tail == g_head {
-            None
-        } else {
-            let e = g_ui_event[g_tail as usize];
-            g_tail = next;
-            Some(e)
-        }
-    }
-}
-
-pub fn mouse_func(button:i32,state:i32,x:i32,y:i32){
-    unsafe {
-        if state!=0{g_mouse_button|=button} else {g_mouse_button&=!button};
-    }
-    push_event(Event::MouseButton  (
-        match button as u32{
-            GLUT_LEFT_BUTTON=>MouseButton::Left,
-            GLUT_RIGHT_BUTTON=>MouseButton::Right,
-            GLUT_MID_BUTTON=>MouseButton::Mid,
-            GLUT_WHEEL_UP=>MouseButton::WheelUp,
-            GLUT_WHEEL_DOWN=>MouseButton::WheelDown,
-            _=>MouseButton::None
-        },
-        match state as u32{GLUT_DOWN=>true,_=>false},
-        [x,y]
-    ));
-    set_mouse_pos(x,y);
-}
-pub fn motion_func(x:i32,y:i32){ set_mouse_pos(x,y);}
-
-pub fn joystick_func(button:c_uint, dx:c_int,dy:c_int,dz:c_int){
-    let s:f32=1.0f32/1024.0f32;
-    unsafe{ g_joystick=((dx as f32 * s, dy as f32 * s),button as c_uint);}
-    println!("JS:{:?} {:?},{:?},{:?}",button,dx,dy,dz);
-}
-
-#[derive(Clone,Debug,Copy)]
-#[repr(i32)]
-pub enum MouseButton{
-    None=0,Left=0x0001,Mid=0x0002,Right=0x0004,WheelUp=0x0008,WheelDown=0x0010,WheelLeft=0x0020,WheelRight=0x0040
-}
+pub struct KeyAt(pub KeyCode,pub bool,pub ViewPos);
+pub type PixelPos=(i32,i32);
+pub type PixelVec=(i32,i32);
+pub type PixelSize=(i32,i32);
+pub type PixelRect=(PixelPos,PixelPos);
+pub type ViewPos=(f32,f32);
+pub fn screen_size()->PixelPos{ unsafe{g_screensize} }
 
 // example,
 //#[derive(Clone,Debug)]
@@ -123,16 +30,20 @@ pub enum Command {
     Move((f32,f32,f32)),
     Create((f32,f32,f32),String),
 }
-
+/// UI events passed to window interfaces.
 #[derive(Clone,Debug,Copy)]
 pub enum Event {
-    None,
-    KeyDown(KeyCode,ScreenPos),
-    KeyUp(KeyCode,ScreenPos),
-    MouseMove(ScreenPos),
-    MouseButton(MouseButton,bool,ScreenPos)
+    NoEvent,
+    Activate(),
+    Deactivate(),
+    KeyEvent(KeyAt),
+    MouseMove(ViewPos),
+    MouseButton(MouseButtons,bool,ViewPos)
 }
+pub use self::Event::*;
 
+/// returned value controls flow between states
+#[repr(u32)]
 pub enum Flow{
     Continue(),
     Redraw(),           /// if not animating, input responses can request redraw
@@ -145,7 +56,7 @@ pub enum Flow{
     Replace(sto<State>),
     Overlay(sto<State>),    /// e.g. popup menu
     SetBackground(sto<State>),  /// equivalent to Replace(x)+Overlay(Self)
-    Root(sto<State>),
+    SetRoot(sto<State>),
     SwapWith(i32),      /// swap with relative indexed state
     Cycle(),            //
     Back(),            //
@@ -153,12 +64,13 @@ pub enum Flow{
     Toggle(),            // rotate top with forward stack
     NewWindow(sto<State>), // multi-windowing
 }
+pub use self::Flow::*;
 
 pub type KeyMappings = FnMut(KeyCode,&str, &mut FnMut()->Flow);
 pub trait State {            //'C' the user defined commands it can respond to.
     fn on_activate(&mut self)   {}
     fn on_deactivate(&mut self) {}
-    fn render(&self,t:f32)      {}
+    fn render(&self,t:f32)      {}  // todo render with presel
     fn info(&self)->String      { String::new()}
     fn update(&mut self,dt:f32)->Flow{Flow::Continue()}   // controller access..
 
@@ -167,17 +79,14 @@ pub trait State {            //'C' the user defined commands it can respond to.
 
     fn key_mappings(&mut self, kmf:&mut KeyMappings){}
 
-    fn on_mouse_move(&mut self,xy:ScreenPos)->Flow {
-        Flow::Passthru()
+    fn on_mouse_move(&mut self,xy:ViewPos)->Flow {
+        Passthru()
     }
-    fn on_mouse_button(&mut self,mb:MouseButton,s:bool,xy:ScreenPos)->Flow {
-        Flow::Passthru()
+    fn on_mouse_button(&mut self,mb:MouseButtons,s:bool,xy:ViewPos)->Flow {
+        Passthru()
     }
-    fn on_key_down(&mut self,kp:KeyAt)->Flow{
-        Flow::Passthru()
-    }
-    fn on_key_up(&mut self,kp:KeyAt)->Flow{
-        Flow::Passthru()
+    fn on_key(&mut self,kp:KeyAt)->Flow{
+        Passthru()
     }
     //fn on_drop(&mut self,f:path::Path,sp:ScreenPos)           {}
     fn command(&mut self, c:Command)->Flow{ Flow::Passthru() }
@@ -186,26 +95,147 @@ pub trait State {            //'C' the user defined commands it can respond to.
     // defaults to calling the fn's
     fn event(&mut self,e:Event)->Flow{
         match e{
-            Event::KeyDown(a,b)=>self.on_key_down(KeyAt(a,b)),
-            Event::KeyUp(a,b)=>self.on_key_up(KeyAt(a,b)),
-            Event::MouseMove(delta)=>self.on_mouse_move(delta),
-            Event::MouseButton(mb,s,pos)=>self.on_mouse_button(mb,s,pos),
-            _=>Flow::Continue(),
+            Activate()=>{self.on_activate();Continue()},
+            Deactivate()=>{self.on_deactivate();Continue()},
+            KeyEvent(k)=>self.on_key(k),
+            MouseMove(delta)=>self.on_mouse_move(delta),
+            MouseButton(mb,s,pos)=>self.on_mouse_button(mb,s,pos),
+            _=>Continue(),
         }
     }
 }
 
+
+static mut g_key:[bool;256]=[false;256];
+static mut g_mouse_button:i32=0;
+static mut g_mouse_pos:PixelPos=(0,0);
+static mut g_joystick:((f32,f32),u32)=((0.0f32,0.0f32),0);
+static mut g_screensize:PixelPos=(1024,1024);
+const  MaxEvent:usize=256;
+static mut g_ui_event:[Event;MaxEvent]=[NoEvent;MaxEvent];
+static mut g_head:i32=0;
+static mut g_tail:i32=0;
+
+
+
+fn reshape_func(x:i32,y:i32){
+    println!("resizing..{:?} {:?}",x,y);
+    unsafe{g_screensize=(x,y); glViewport(0,0,x,y);}
+
+   // panic!();
+}
+fn keyboard_func_sub(key:u8,isdown:bool,x:i32,y:i32){
+    unsafe{
+        g_key[key as usize]=isdown;
+        let kp=KeyAt(key as KeyCode, isdown, to_viewpos((x,y)));
+        push_event(KeyEvent(kp));
+    }
+}
+
+fn keyboard_func(key:u8, x:i32,y:i32){
+    keyboard_func_sub(key,true,x,y);
+}
+fn special_func_sub(key:GLuint, isdown:bool,x:i32,y:i32){
+    assert!((key as u32&0xff) == key as u32);
+    unsafe{
+        let kp=KeyAt(key as u8 as KeyCode,isdown, to_viewpos((x,y)));
+        push_event(KeyEvent(kp));
+        g_key[key as usize]=isdown;
+    }
+}
+fn special_func(key:GLuint, x:i32,y:i32){
+    special_func_sub(key,true,x,y);
+}
+fn special_up_func(key:GLuint, x:i32,y:i32){
+    special_func_sub(key,false,x,y);
+}
+
+//pub fn modifiers()->GLuint{glutGetModifiers()}
+
+fn keyboard_up_func(key:u8, x:i32,y:i32){
+    keyboard_func_sub(key,false,x,y);
+}
+fn divf32(a:i32,b:i32)->f32{a as f32 / b as f32}
+fn to_viewpos(s:PixelPos)->ViewPos{
+    unsafe{
+    (   divf32(s.0,g_screensize.0)*2.0f32-1.0f32,
+        -(divf32(s.1,g_screensize.1)*2.0f32-1.0f32)
+    )}
+}
+fn set_mouse_pos(x:i32,y:i32){unsafe{g_mouse_pos=(x,y)};}
+fn push_event(e:Event){
+    unsafe {
+        let next = (g_head + 1) & ((MaxEvent - 1) as i32);
+        if next != g_tail {
+            g_ui_event[g_head as usize] = e;
+            g_head=next;
+        } else {
+            println!("buffer full,lost event");
+        }
+    }
+}
+fn pop_event()->Option<Event>{
+    unsafe {
+        let next = (g_tail + 1) & ((MaxEvent - 1) as i32);
+        if g_tail == g_head {
+            None
+        } else {
+            let e = g_ui_event[g_tail as usize];
+            g_tail = next;
+            Some(e)
+        }
+    }
+}
+
+fn mouse_func(button:i32,state:i32,x:i32,y:i32){
+    unsafe {
+        if state!=0{g_mouse_button|=button} else {g_mouse_button&=!button};
+    }
+    push_event(MouseButton  (
+        match button as u32{
+            GLUT_LEFT_BUTTON    =>LeftButton,
+            GLUT_RIGHT_BUTTON   =>RightButton,
+            GLUT_MID_BUTTON =>MidButton,
+            GLUT_WHEEL_UP   =>WheelUp,
+            GLUT_WHEEL_DOWN =>WheelDown,
+            _=>NoButton
+        },
+        match state as u32{GLUT_DOWN=>true,_=>false},
+        to_viewpos((x,y))
+    ));
+    set_mouse_pos(x,y);
+}
+fn motion_func(x:i32,y:i32){ set_mouse_pos(x,y);}
+
+fn joystick_func(button:c_uint, dx:c_int,dy:c_int,dz:c_int){
+    let s:f32=1.0f32/1024.0f32;
+    unsafe{ g_joystick=((dx as f32 * s, dy as f32 * s),button as c_uint);}
+    println!("JS:{:?} {:?},{:?},{:?}",button,dx,dy,dz);
+}
+
+#[derive(Clone,Debug,Copy)]
+#[repr(i32)]
+pub enum MouseButtons{
+    NoButton=0,LeftButton=0x0001,MidButton=0x0002,RightButton=0x0004,WheelUp=0x0008,WheelDown=0x0010,WheelLeft=0x0020,WheelRight=0x0040
+}
+pub use self::MouseButtons::*;
+
+
 type WindowId=c_int;
 //static mut g_windows:*mut Vec<(Box<Window>,WindowId)> ;
 
-pub fn render_begin(){
+fn render_begin(){
     unsafe {
         glClearColor(0.5f32, 0.5f32, 0.5f32, 1.0f32);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
     }
 }
-pub fn render_end(){
+fn render_end(){
     unsafe {
         glFlush();
         glutSwapBuffers();
@@ -214,9 +244,9 @@ pub fn render_end(){
 type IsOverlay=bool;
 type Windows=(Vec<(Box<State>,IsOverlay)>,WindowId);
 
-pub fn render_null(){}
+fn render_null(){}
 
-pub fn process_flow(next:Flow,wins:&mut Windows){
+fn process_flow(next:Flow,wins:&mut Windows){
 // process state flow.
     //assert!(unsafe {glutGetWindow()}==wins.1);
 
@@ -224,19 +254,19 @@ pub fn process_flow(next:Flow,wins:&mut Windows){
     if top==0 {return}
     match next {
         //todo - spawn , new glut window.
-        Flow::Pop() => { wins.0.pop(); }
-        Flow::Push(w) => { wins.0.push((w, false)); }
-        Flow::Replace(w) => { wins.0[top - 1] = (w, false); }
-        Flow::Overlay(w) => { wins.0[top - 1] = (w, true); }
-        Flow::SwapWith(i)=> wins.0 .swap((top-1) as usize,(top as i32 -1+i) as usize),
-        Flow::SetBackground(w)=>{ // current becomes an overlay on 'w'
+        Pop() => { wins.0.pop(); }
+        Push(w) => { wins.0.push((w, false)); }
+        Replace(w) => { wins.0[top - 1] = (w, false); }
+        Overlay(w) => { wins.0[top - 1] = (w, true); }
+        SwapWith(i)=> wins.0 .swap((top-1) as usize,(top as i32 -1+i) as usize),
+        SetBackground(w)=>{ // current becomes an overlay on 'w'
             wins.0.push((w,false));
             let l=wins.0 .len();
             wins.0 .swap(l-1,l-2);
 
             wins.0[l-2].1=true;
         }
-        Flow::Root(w) => {
+        SetRoot(w) => {
             wins.0 .truncate(0);//discard all
             wins.0 .push((w, false));
         }
@@ -251,7 +281,7 @@ pub fn process_flow(next:Flow,wins:&mut Windows){
 }
 
 
-pub fn render_and_update(wins:&mut Windows){
+fn render_and_update(wins:&mut Windows){
     unsafe{
         //render.
         let top=wins.0 .len();
@@ -273,8 +303,8 @@ pub fn render_and_update(wins:&mut Windows){
                 let mut y = 0.9f32;
                 let win = &mut wins.0[i];
                 win.0.key_mappings(&mut move |k, name, _| {
-                    bsp::bspdraw::char_at(&(-0.95f32, y, 0.0f32), 0x00ff00, k);
-                    bsp::bspdraw::string_at(&(-0.9f32, y, 0.0f32), 0x00ff00, name);
+                    bspdraw::char_at(&(-0.95f32, y, 0.0f32), 0x00ff00, k);
+                    bspdraw::string_at(&(-0.9f32, y, 0.0f32), 0x00ff00, name);
                     y -= 0.05f32;
                 });
             }
@@ -284,15 +314,13 @@ pub fn render_and_update(wins:&mut Windows){
         }
 
 
-        //update
-        let top=wins.0 .len();
-        if top>0 {
-
-            if let Some(k) = g_keypress {
-
+        // Update: process all the events.
+        while let Some(e)=pop_event() {
+            let top=wins.0 .len();
+            if top>0 {
                 process_flow(
                     {let win = &mut wins.0[top - 1];
-                        win.0 .on_key_down(KeyAt(k as KeyCode, g_keypress_pos))}
+                        win.0 .event(e)}
                     ,wins);
             }
         }
@@ -334,13 +362,13 @@ pub fn run_loop(mut w:sto<State>) {
         let win=glutCreateWindow(c_str("world main loop"));
         //		glewInit(); //TODO- where the hell is glewInit. -lGLEW isn't found
         glDrawBuffer(GL_BACK);
-        glutReshapeWindow(g_screensize[0],g_screensize[1]);
+        glutReshapeWindow(g_screensize.0,g_screensize.1);
 
         glutDisplayFunc(render_null as *const u8);
         glutKeyboardFunc(keyboard_func as *const u8);
         glutKeyboardUpFunc(keyboard_up_func as *const u8);
         glutMouseFunc(mouse_func as *const u8);
-        glutMouseFunc(reshape_func as *const u8);
+        glutReshapeFunc(reshape_func as *const u8);
         glutMotionFunc(motion_func as *const u8);
         glutIdleFunc(idle_func as *const u8);
         glutJoystickFunc(joystick_func as *const u8,16);
